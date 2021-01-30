da5a3fab6b0fe9daee176136dbf902d10f69fc37
Yaiza16 <yaizavallejo16@gmail.com>
2021-01-30 20:06:39 +0100

    Funciones avanza + codifica

---
+++ MyEnigma.py
@@ -0,0 +1,39 @@
+import random
+
+rotor_pruebas = [('A', 'I'), ('B', 'C'), ('C', 'S'), ('D', 'E'), ('E', 'V'), ('F', 'K'), ('G', 'L'), ('H', 'Z'), ('I', 'R'), ('J', 'A'), ('K', 'N'), ('L', 'Ñ'), ('M', 'D'), ('N', 'U'), ('Ñ', 'J'), ('O', 'F'), ('P', 'W'), ('Q', 'M'), ('R', 'P'), ('S', 'B'), ('T', 'O'), ('U', 'Q'), ('V', 'X'), ('W', 'T'), ('X', 'Y'), ('Y', 'H'), ('Z', 'G')]
+
+class Rotor():
+
+    def __init__(self, abecedario="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"):
+        self.abecedario = abecedario
+        otrasLetras = list(self.abecedario)
+        self.rotor=[]
+        for l in self.abecedario:
+            n = random.randrange(len(otrasLetras))
+            self.rotor.append((l, otrasLetras[n]))
+            otrasLetras.pop(n)
+
+        self.rotorC = self.rotor[:]
+
+    
+    def codifica(self, letra):
+        try:
+            pLetra = self.abecedario.index(letra)
+            return self.rotorC[pLetra][1]
+        except ValueError:
+            print("{} no pertenece al abecedario".format(letra))
+
+    
+    def posicionInicial(self, letra):
+        position = self.abecedario.index(letra)
+        self.rotorC = self.rotor[position:]+self.rotor[:position]
+        return self.rotorC
+    
+    def avanza(self):
+        self.rotorC = self.rotorC[1:]+self.rotorC[:0]
+
+
+r = Rotor()
+print(r.codifica('S'))
+
+print(r.posicionInicial('G'))
